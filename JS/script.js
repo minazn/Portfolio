f5df5aab4bcce6fc05457b351bc2ce3b8b3d4fcf
Neslihan Minaz <minaz_nesli@hotmail.com>
2021-07-26 19:56:55 +0200

    polish design

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 const titles = ["Portfolio","Technologys","About me"];
 const titles_id = ["#portfolio","#technologys", "#about"];
 
+// responsive hamburger navbar
 function toggleMenu() {
 	$('.hamburger-shell').toggleClass('menu-open');
   $('#hamburger').toggleClass('menu-open');
@@ -11,21 +12,21 @@ $(function() {
   $('.hamburger-shell').click(toggleMenu);
 
   for (let i=0; i<titles.length; i++){
+    //for the sectiontitle animation, I need to split the strings in letters
     let split_title = titles[i].split("");
-    $(titles_id[i]).html(`<li>[</li>`);
+    //creating lists of letters
     for (let j=0; j<split_title.length;j++){
-      if (split_title[j] == " "){
+      if (split_title[j] == " "){ //otherwise it will return "Aboutme" instead of "About me"
         $(titles_id[i]).append(`<li class="ghost">` + " &nbsp " + `</li>`);;
       }else{
       $(titles_id[i]).append(`<li class="ghost">` + split_title[j] + `</li>`);
       }
     }
-    $(titles_id[i]).append(`<li>]</li>`);
 
   }
-
+    //scroll animation
     $(window).scroll(function() {
-      var scroll = $(window).scrollTop() + 400;
+      var scroll = $(window).scrollTop() + 750;
       for (let val of titles_id){
         if (scroll >= $(val).position().top){
           $(val).removeClass("hidden");
