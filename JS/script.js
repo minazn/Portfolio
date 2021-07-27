7dab6d80e35dfe36967e5f396963974164ee126b
Neslihan Minaz <minaz_nesli@hotmail.com>
2021-07-27 14:48:21 +0200

    writing technology cards with JS (only functionalty, not content)

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,71 @@
-const titles = ["Portfolio","Technologys","About me"];
-const titles_id = ["#portfolio","#technologys", "#about"];
+const titles = ["Portfolio","What are Codereviews?","Technologys","About me"];
+const titles_id = ["#portfolio","#codereview","#technologys", "#about"];
+
+const technologys = [{
+  title: "C/C++",
+  id: "c",
+  description: "1"
+},{
+  title: "C/C++",
+  id: "c++",
+  description: "2"
+},{
+  title: "Matlab",
+  id: "matlab",
+  description: "3"
+},{
+  title: "R",
+  id: "r",
+  description: "4"
+},{
+  title: "LaTex",
+  id: "latex",
+  description: "5"
+},{
+  title: "Maple",
+  id: "maple",
+  description: ""
+},{
+  title: "Wordpress",
+  id: "wordpress",
+  description: ""
+},{
+  title: "Angular",
+  id: "angular",
+  description: ""
+},{
+  title: "HTML",
+  id: "html",
+  description: ""
+},{
+  title: "JavaScript",
+  id: "js",
+  description: ""
+},{
+  title: "TypeScript",
+  id: "ts",
+  description: ""
+},{
+  title: "CSS3 & SCSS",
+  id: "css",
+  description: ""
+}];
 
 // responsive hamburger navbar
 function toggleMenu() {
 	$('.hamburger-shell').toggleClass('menu-open');
   $('#hamburger').toggleClass('menu-open');
+}
+
+function writeTechnologyDescription(obj){
+  document.getElementById("technology-title").innerHTML = obj.title;
+
+  document.getElementById("technology-description").innerHTML = obj.description;
+}
+
+for (let val of technologys){
+  document.getElementById(val.id).addEventListener("click", function(){
+    writeTechnologyDescription(val)});
 }
 
 $(function() {
@@ -36,18 +97,9 @@ $(function() {
       }
     });
 
-    // $("#technology-tags").tagcloud({
-    //     size: {
-    //       start:12,
-    //       end: 55,
-    //       unit: "pt"
-    //     },
-    //     color: {
-    //       start: "#aaa",
-    //       end: "#000"
-    //     }
-    // })
   });
+
+ console.log(technologys[0].id);
 
  
  
